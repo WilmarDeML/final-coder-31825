ce4c41ccd0c64929f0bed4a20538697f0431374a
Wilmar Zapata <wavilla@stefanini.com>
2022-05-30 01:25:24 -0500

    Diseñar rutas a implementar en router /api/productos

--- src/routers/routerProductos.js
+++ src/routers/routerProductos.js
@@ -2,4 +2,24 @@ import express from "express"
 
 const routerProductos = express.Router()
 
+routerProductos.get("/", (req, res) => {
+    res.send({funcion: 'Listará todos los productos disponibles'})
+})
+
+routerProductos.get("/:id", (req, res) => {
+    res.send({funcion: 'Listará un producto por su id'})
+})
+
+routerProductos.post("/", (req, res) => {
+    res.send({funcion: 'Incorporará productos al listado', ojo: 'Sólo admin'})
+})
+
+routerProductos.put("/:id", (req, res) => {
+    res.send({funcion: 'Actualizará un producto por su id', ojo: 'Sólo admin'})
+})
+
+routerProductos.delete("/:id", (req, res) => {
+    res.send({funcion: 'Borrará un producto por su id', ojo: 'Sólo admin'})
+})
+
 export default routerProductos
