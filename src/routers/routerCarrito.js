3985f40f7da86a4d0dc55560e2b25663ee6daea7
Wilmar Zapata <wavilla@stefanini.com>
2022-06-01 00:18:45 -0500

    Separar controladores de la capa de ruteo en ctrlApiCarritos.js

--- src/routers/routerCarrito.js
+++ src/routers/routerCarrito.js
@@ -1,41 +1,17 @@
 import express from "express"
-import ContenedorArchivo from "../containers/ContenedorArchivo.js"
-
-const contenedorCarritos = new ContenedorArchivo('carritos')
-const contenedorProductos = new ContenedorArchivo('productos')
+import ctrlApiCarrito from "../controllers/ctrlApiCarrito.js"
 
 const routerCarrito = express.Router()
 
-routerCarrito.post('/', async (req, res) => {
-    res.send(await contenedorCarritos.save({ productos: [] }))
-})
+routerCarrito.post('/', ctrlApiCarrito.crearCarrito)
 
-routerCarrito.post('/:idCarrito/productos', async (req, res) => {
-    const producto = await contenedorProductos.getById(req.body.id)
-    const carrito = await contenedorCarritos.getById(req.params.idCarrito)
-    if (!carrito || !producto) {
-        return res.send({ error: "Carrito o Producto no encontrado" })
-    }
-    carrito.productos.push(producto)
-    await contenedorCarritos.update(carrito.id, carrito)
-    res.send(producto)
-})
+routerCarrito.post('/:idCarrito/productos', ctrlApiCarrito.agregarProductoAlCarrito)
 
-routerCarrito.get('/:id/productos', async (req, res) => {
-    const carrito = await contenedorCarritos.getById(req.params.id)
-    res.send(carrito?.productos)
-})
+routerCarrito.get('/:id/productos', ctrlApiCarrito.listarProductosDeUnCarritoPorId)
 
-routerCarrito.delete('/:idCarrito/productos/:idProd', async (req, res) => {
-    const carrito = await contenedorCarritos.getById(req.params.idCarrito)
-    carrito.productos = carrito.productos.filter(producto => producto.id !== req.params.idProd)
-    await contenedorCarritos.update(carrito.id, carrito)
-    res.send(carrito.productos)
-})
+routerCarrito.delete('/:idCarrito/productos/:idProd', ctrlApiCarrito.retirarProductoDeUnCarritoPorId)
 
-routerCarrito.delete('/:id', (req, res) => {
-    res.send({funcion: 'Vaciará un carrito'})
-})
+routerCarrito.delete('/:id', ctrlApiCarrito.vaciarCarrito)
 
 export default routerCarrito
 
